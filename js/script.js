1007300baf5e68b310d40953d18235a28acdfbf0
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-14 02:07:03 +0100

    Cancellato check KM, aggiunto type=number a KM, aggiunto output nel ticket

--- js/script.js
+++ js/script.js
@@ -30,16 +30,25 @@ form.addEventListener("submit", (event) => {
     let price = parseFloat(km.value) * 0.21;
     let rate = "tariffa standard";
 
-    // CHECK DI KM
-    if (isNaN(km) || km <= 0){
-        
+    // APPLICO GLI SCONTI
+    if (age === "underage"){
+        price = price - (price * 20) / 100;
+        rate = "Minor discount";
+    } else if (age === "senior"){
+        price = price - (price * 40) / 100;;
+        rate = "Senior discount";
     }
-
+    //RENDO VISIBILE IL BIGLIETTO
     viewTicket.classList.remove("d-none");
+
+    //TABELLA OUTPUT
+    oName.innerText = name;
+    oOffer.innerText = rate;
+    oPrice.innerText = `${price.toFixed(2)} €`;
 })
 
 // SVUOTO IL FORM E IL TICKET 
 btnCancel.addEventListener("click", () => {
     form.reset();
-    viewTicket.classList.add("d-none")
+    viewTicket.classList.add("d-none");
 })
